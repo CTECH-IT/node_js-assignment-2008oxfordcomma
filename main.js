1552ff49f2d8bf8d68a2fc6a3aa7d44d2820c8b3
Dre <2008oxfordcomma@gmail.com>
2022-04-25 17:10:27 -0500

    Added user email and password variables

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var nodemailer = require('nodemailer');
+var myEmail = 'placeHolderEmail';
+var myPassword = 'placeHolderPassword';
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'myEmail',
-    pass: 'myPassword'
+    user: myEmail,
+    pass: myPassword,
   }
 });
 
 var mailOptions = {
-  from: 'fromEmail',
-  to: 'toEmail',
+  from: myEmail,
+  to: 'jobartucz@isd535.org',
   subject: 'Node JS Email',
-  html: '<p>Hey Mr.Bartucz<br>This is my email made with node.js.</p>',
+  html: '<p>Hey Mr.Bartucz,<br /><br />This is my email sent to you with Node.js.<br /><br />Sincerely,<br>Dre Harm</p>',
 };
 
 transporter.sendMail(mailOptions, function(error, info){
